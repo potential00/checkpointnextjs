a2f85eec975c83bf3f80e5c048cf21f42f812710
potential00 <158043435+potential00@users.noreply.github.com>
2024-07-12 16:54:23 +0100

    Update Header.jsx

--- Checkpoint-Next-JS/components/Header.jsx
+++ Checkpoint-Next-JS/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         {/*logo */}
         <Link href="/">
         <h1 className="text-4xl font-semibold">
-            Jordan <span className="text-accent">.</span>
+            DPOTENTIAL <span className="text-accent">.</span>
         </h1>
         </Link>
 
